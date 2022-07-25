9cb6d05cff421bfeca0955d761b7ad9d035e24a3
yocuml19 <yocuml19@tamu.edu>
2022-07-24 22:11:57 -0500

    added the map and calculates sea level rise

---
+++ Texas_sea_rise.m
@@ -0,0 +1,26 @@
+% take user input for the year they want projected sea rise
+yearx = input("Enter the year you would like to know how much the sea is projected to rise by ");
+% get the current year
+d = datetime('today');
+yearc = year(d);
+% subtract yearx - yearc to get the difference
+year_diff = yearx - yearc;
+% use the average projection of 10mm/yr from 
+% https://www-sciencedirect-com.srv-proxy1.library.tamu.edu/science/article/pii/S0025322713002636
+% multiply the year difference by 10mm/yr to get the amount it will rise on
+% average
+rise_mm = 10 * year_diff;
+% convert millimeters to feet
+rise = rise_mm / 304.8;
+% if less than 1 ft convert to inches
+if rise < 1
+    rise = rise * 12;
+end
+% create the map
+geolimits([25 31],[-97 -94])
+geobasemap streets
+
+labelLat = 28;
+labelLon = 94;
+textm(labelLat, labelLon, 'rise');
+
